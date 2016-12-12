0455c40def42cb80edeff04fb88240cc535c3f89
maxgrok <maxgrok2011@gmail.com>
2016-12-12 10:36:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,86 @@
 const app = "I don't do much."
+
+function fatBastard(meal){
+	function whatsForDinner(){
+		if(meal === 'Mini-Me'){
+			console.log('The wee man is in my belly!');
+		}
+		else{
+			console.log(`I'm eatin a bit of ${meal}! Burp.`);
+		}
+	}
+
+	function digest(){
+		meal = undefined;
+	}
+
+	return {
+		whatsForDinner,
+		digest
+	};
+}
+
+const whatsForDinner = fatBastard('kobe beef');
+whatsForDinner();
+
+const whatsInHisTummy = fatBastard('kobe beef');
+whatsInHisTummy();
+
+const { whatsForDinner, digest } = fatBastard('ribeye');
+whatsForDinner();
+digest();
+whatsForDinner();
+
+function raspyDoorGuy(){
+	const password = 'yarr';
+
+	function givePassword(givenPassword){
+		if(givenPassword === password){
+			console.log('Ye may enter.');
+		}
+		else{
+			console.log('Begone, landlubber!');
+		}
+	}
+
+	return {
+		givePassword
+	};
+}
+
+const { givePassword } = raspyDoorGuy();
+givePassword('kittens');
+
+function raspyDoorGuy(){
+	const password = 'yarr';
+
+	function givePassword(givenPassword){
+		if(givenPassword === password){
+			console.log('Ye may enter.');
+		}
+		else{
+			console.log('Begone, landlubber!');
+		}
+	}
+
+	function willBreakPrinciples(bribeAmount){ 
+		return bribeAmount >= 50;
+	}
+
+	function bribe(amount){ 
+		if(willBreakPrinciples(amount)){ 
+			return password;
+		}
+		else { 
+			console.log("Pssht. That won't work.");
+		}
+	}
+
+	return { 
+		givePassword,
+		bribe
+	};
+}
+
+const { bribe } = raspyDoorGuy();
+bribe(80); // returns 'yarr'
